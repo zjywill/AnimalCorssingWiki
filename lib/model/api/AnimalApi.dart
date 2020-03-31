23ba7840ab1321c2e7a7b8fc6470b73fea2f20f7
zjywill <zjywill@hotmail.com>
2020-03-31 14:40:58 +0800

    add cache

--- lib/model/api/AnimalApi.dart
+++ lib/model/api/AnimalApi.dart
@@ -1,5 +1,5 @@
 import 'dart:developer';
-
+import 'dart:io';
 import 'package:animalcorssingwiki/model/pojo/Bug.dart';
 import 'package:animalcorssingwiki/model/pojo/Fish.dart';
 
@@ -7,6 +7,7 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
 
 class AnimalApi {
 //  Future<http.Response> fetchForecast() {
@@ -17,11 +18,17 @@ class AnimalApi {
 
   Future<Fish> fetchFish(pole) async {
     log('pole: $pole');
+    String cacheName = "fish$pole";
+    String contents = await readCache(cacheName);
+    if (contents.isNotEmpty) {
+      return Fish.fromJson(json.decode(contents));
+    }
     final response =
         await http.get('http://li.zerob13.com:8080/function/ac/$pole/fish');
     log('fetchFish response $response');
     if (response.statusCode == 200) {
       log('fetchFish response 200');
+      await writeCache(cacheName, response.body);
       return Fish.fromJson(json.decode(response.body));
     } else {
       log('fetchFish Failed');
@@ -31,15 +38,46 @@ class AnimalApi {
 
   Future<Bug> fetchBug(pole) async {
     log('pole: $pole');
+    String cacheName = "bugs$pole";
+    String contents = await readCache(cacheName);
+    if (contents.isNotEmpty) {
+      return Bug.fromJson(json.decode(contents));
+    }
     final response =
-    await http.get('http://li.zerob13.com:8080/function/ac/$pole/bug');
+        await http.get('http://li.zerob13.com:8080/function/ac/$pole/bug');
     log('fetchBug response $response');
     if (response.statusCode == 200) {
       log('fetchBug response 200');
+      await writeCache(cacheName, response.body);
       return Bug.fromJson(json.decode(response.body));
     } else {
       log('fetchBug Failed');
       throw Exception('Failed to fetch Bug');
     }
+  }
+
+  Future<File> writeCache(String name, String content) async {
+    final file = await _localFile(name);
+    return file.writeAsString(content);
+  }
+
+  Future<String> readCache(String name) async {
+    try {
+      final file = await _localFile(name);
+      String contents = await file.readAsString();
+      return contents;
+    } catch (e) {
+      return "";
+    }
+  }
+
+  Future<String> get _localPath async {
+    final directory = await getTemporaryDirectory();
+    return directory.path;
+  }
+
+  Future<File> _localFile(String name) async {
+    final path = await _localPath;
+    return File('$path/$name.cache');
   }
 }
